4589a302e745d35f101c14b87d7f81662c2ea7b6
renny5120 <renny5120@gmail.com>
2024-01-17 11:23:38 -0800

    fixed correct operation after equals

--- javascript.js
+++ javascript.js
@@ -3,38 +3,38 @@ let num = null;
 let op = null;
 let displayValue;
 
-function add(a,b){
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b){
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 }
 
-function operate(operator, a, b){
+function operate(operator, a, b) {
     let value;
     a = parseFloat(a);
     b = parseFloat(b);
-    switch(operator){
+    switch (operator) {
         case '+':
-            value = add(a,b);
+            value = add(a, b);
             break;
         case '-':
-            value = subtract(a,b);
+            value = subtract(a, b);
             break;
         case '*':
-            value = multiply(a,b);
+            value = multiply(a, b);
             break;
         case '/':
-            value = divide(a,b);
+            value = divide(a, b);
             break;
     }
     return value;
@@ -47,18 +47,18 @@ let buttons = document.querySelector('.buttons');
 buttons.addEventListener('click', (e) => {
     let element = e.target;
     const selectedClass = element.parentElement.classList[0];
-    switch(selectedClass){
+    switch (selectedClass) {
         case 'numbers':
 
             // Clears display only after input
-            if (afterOp){
+            if (afterOp) {
                 displayText.textContent = '';
                 afterOp = false;
             }
-            
+
             // Make sure display cannot start with 0
-            if (displayText.textContent == '0' && element.textContent === '0') {break;}
-            if (displayText.textContent == '' && element.textContent === '0') {break;}
+            if (displayText.textContent == '0' && element.textContent === '0') { break; }
+            if (displayText.textContent == '' && element.textContent === '0') { break; }
 
             // adds number to display
             displayText.textContent += element.textContent;
@@ -70,27 +70,34 @@ buttons.addEventListener('click', (e) => {
                 num1 = null;
                 num2 = null;
                 op = null;
-            }else if(element.textContent === '=') {
+            } else if (element.textContent === '=') {
                 num2 = displayText.textContent;
                 console.log(`OP: ${op}`);
                 console.log(`num1: ${num1}`);
                 console.log(`num2: ${num2}`);
-                num1 = operate(op,num1,num2);
+                num1 = operate(op, num1, num2);
                 console.log(num1);
                 displayText.textContent = num1;
+                op = null;
                 afterOp = true;
-            }else {
-                if (num1 === null){
+            } else {
+                if (num1 === null || op == null) {
                     num1 = displayText.textContent;
                 } else {
                     num2 = displayText.textContent;
-                    console.log(num2);
+                    console.log(`OP: ${op}`);
+                    console.log(`num1: ${num1}`);
+                    console.log(`num2: ${num2}`);
+                    num1 = operate(op, num1, num2);
+                    displayText.textContent = num1;
+                    afterOp = true;
+                    // console.log(num2);
                 }
-                
+                if (num1 !== null) 
                 op = element.textContent;
                 afterOp = true;
                 // num1 = num2;
-                
+
             }
             break;
         default:
